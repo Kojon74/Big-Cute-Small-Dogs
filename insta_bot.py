1957508a1516055788538f85878b4e6871e61822
Kojon74 <52554666+Kojon74@users.noreply.github.com>
2020-04-16 16:24:07 +0900

    Update insta_bot.py

--- insta_bot.py
+++ insta_bot.py
@@ -1,5 +1,5 @@
 from instapy import InstaPy
 
-session = InstaPy(username="chickenjunjj@gmail.com", password="Jojon524")
+session = InstaPy(username="-", password="-")
 session.login()
 session.like_by_tags(["bmw", "mercedes"], amount=5)
